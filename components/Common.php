0840c19c6c095dd2e338c03efd420d69a149d308
evgeny <svd22286@gmail.com>
2016-02-05 02:49:35 +0300

    added `Common` component

---
+++ components/Common.php
@@ -0,0 +1,20 @@
+<?php
+namespace app\components;
+
+use \Yii;
+use \yii\base\Component;
+
+class Common extends Component {
+    
+    public static function substrBoundary($str, $length) {
+        $str = explode(' ', $str);
+        $return = '';
+        $i = $len = 0;
+        while($len <= $length) {
+            $return .= $str[$i].' ';
+            $len += strlen($str[$i]) + 1;
+            $i++;
+        }
+        return $return;
+    }
+}
